6c5aec25661768460cb8ed4e00071613a76e6acc
11bus11 <02linfor01@gmail.com>
2024-12-05 19:17:40 +0100

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -1,5 +1,7 @@
+//01:02:00 2024-11-12
+
 class catTressure {
-    //private int pos;//
+
     public catTressure() {
     }
     catTressure game = new catTressure();
@@ -19,7 +21,37 @@ class DungeonRoom {
         room1.roomDesc = "thed";
         room1.posY = 2;
         room1.posX = 1;
-        System.out.println("beep beep running " + room1.roomDesc);
+
+        DungeonRoom room2 = new DungeonRoom();
+        room2.roomDesc = "thefgsgrd";
+        room2.posY = 2;
+        room2.posX = 1;
+
+        DungeonRoom room3 = new DungeonRoom();
+        room3.roomDesc = "thed";
+        room3.posY = 2;
+        room3.posX = 1;
+
+        DungeonRoom room4 = new DungeonRoom();
+        room4.roomDesc = "thed";
+        room4.posY = 2;
+        room4.posX = 1;
+
+        DungeonRoom room5 = new DungeonRoom();
+        room5.roomDesc = "thed";
+        room5.posY = 2;
+        room5.posX = 1;
+
+        DungeonRoom room6 = new DungeonRoom();
+        room6.roomDesc = "thed";
+        room6.posY = 2;
+        room6.posX = 1;
+        System.out.println("beep beep running " + room2.roomDesc);
+        
+        
+    }
+    public String getRoom(){
+        return room1;
     }
 }
 
