27ca55c165c5f2c5eafe03b9b9ddfe3afbed475f
11bus11 <02linfor01@gmail.com>
2024-12-15 22:03:13 +0100

    created the parent classes

--- CatTreasure/Item.java
+++ CatTreasure/Item.java
@@ -1,5 +1,13 @@
 package CatTreasure;
 
 public class Item {
-    
+    boolean status;
+
+    public Item(String itemName, boolean status) {
+        this.status = status;
+    }
+
+    public boolean getStatus() {
+        return this.status;
+    }
 }
--- CatTreasure/Mob.java
+++ CatTreasure/Mob.java
@@ -1,5 +1,31 @@
 package CatTreasure;
 
 public class Mob {
-    
+    int mobId;
+    boolean status;
+    int health;
+    int damage;
+
+    public Mob(int mobId, boolean status, int health, int damage) {
+        this.mobId = mobId;
+        this.status = status;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public int getMobId() {
+        return this.mobId;
+    }
+
+    public boolean getStatus() {
+        return this.status;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
 }
