ffaf495aefa09bfd936f3ba6bdbff7be102255ff
11bus11 <02linfor01@gmail.com>
2025-01-04 11:53:17 +0100

    removed unnecessary code

--- CatTreasure/PlayGame.java
+++ CatTreasure/PlayGame.java
@@ -9,7 +9,7 @@ import java.lang.Math;
 public class PlayGame {
     public static void main(String[] args) {
         //create mobs
-        Vaccum vaccum = new Vaccum(true, "The scary vaccum", true, 20, 1, false, 0, 3);
+        Vaccum vaccum = new Vaccum(true, "The scary vaccum", true, 20, 1, 0, 3);
         Leaves leaf1 = new Leaves(false, "Bob the leaf", true, 5, 1);
         Leaves leaf2 = new Leaves(false, "Bill the leaf", true, 5, 1);
         
@@ -471,9 +471,8 @@ public class PlayGame {
         if (lockedDoor != null) {
             if (lockedDoor.locked) {
             locked = true;
+            }
         }
-        }
-        
         return locked;
     }
 }
--- CatTreasure/Vaccum.java
+++ CatTreasure/Vaccum.java
@@ -1,19 +1,13 @@
 package CatTreasure;
 
 public class Vaccum extends Mob {
-    boolean superAttack;
     int superCharge;
     int superDamage;
 
-    public Vaccum(boolean boss, String mobName, boolean status, int health, int damage, boolean superAttack, int superCharge, int superDamage) {
+    public Vaccum(boolean boss, String mobName, boolean status, int health, int damage, int superCharge, int superDamage) {
         super(boss, mobName, status, health, damage);
-        this.superAttack = superAttack;
         this.superCharge = superCharge;
         this.superDamage = superDamage;
-    }
-
-    public boolean getSuperAttack() {
-        return this.superAttack;
     }
 
     public int getSuperCharge() {
